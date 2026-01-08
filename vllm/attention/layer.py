64e5fcf1df3683ab586abb16d1a4824cef6785ec
maoxx241 <maomaoyu870@gmail.com>
2026-01-08 17:20:00 +0800

    init model and dsa

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -967,3 +967,153 @@ direct_register_custom_op(
     fake_impl=unified_mla_attention_with_output_fake,
     dispatch_key=current_platform.dispatch_key,
 )
+
+class DSAAttention(nn.Module, AttentionLayerBase):
+    """Multi-Head Latent Attention layer.
+
+    This class takes query, and compressed key/value tensors as input.
+    The class does the following:
+
+    1. Store the input key and value tensors in the KV cache.
+    2. Perform (multi-head/multi-query/grouped-query) attention.
+    3. Return the output tensor.
+    """
+
+    def __init__(
+        self,
+        # num_heads: int,
+        # scale: float,
+        # qk_nope_head_dim: int,
+        # qk_rope_head_dim: int,
+        # v_head_dim: int,
+        # q_lora_rank: int | None,
+        # kv_lora_rank: int,
+        # kv_b_proj: ColumnParallelLinear,
+        # cache_config: CacheConfig | None = None,
+        # quant_config: QuantizationConfig | None = None,
+        # prefix: str = "",
+        # use_sparse: bool = False,
+        # indexer: object | None = None,
+        **extra_impl_args,
+    ):
+        super().__init__()
+        # self.num_heads = num_heads
+        # self.scale = scale
+        # self.qk_nope_head_dim = qk_nope_head_dim
+        # self.qk_rope_head_dim = qk_rope_head_dim
+        # self.v_head_dim = v_head_dim
+        # self.q_lora_rank = q_lora_rank
+        # self.kv_lora_rank = kv_lora_rank
+        # self.head_size = kv_lora_rank + qk_rope_head_dim
+        # self.layer_name = prefix
+        self.head_size = 256
+
+        if cache_config is not None:
+            kv_cache_dtype = cache_config.cache_dtype
+            block_size = cache_config.block_size
+            calculate_kv_scales = cache_config.calculate_kv_scales
+        else:
+            kv_cache_dtype = "auto"
+            block_size = 16
+            calculate_kv_scales = False
+
+        # Initialize KV cache quantization attributes
+        _init_kv_cache_quant(
+            self, quant_config, prefix, kv_cache_dtype, calculate_kv_scales
+        )
+
+        dtype = torch.get_default_dtype()
+        print(f'??????????????????????????????????????????')
+        self.attn_backend = get_attn_backend(
+            self.head_size,
+            dtype,
+            kv_cache_dtype,
+            block_size,
+            use_mla=False,
+            use_sparse=True,
+            use_compress=True,
+        )
+
+        if (
+            cache_config is not None
+            and cache_config.enable_prefix_caching
+            and vllm_is_batch_invariant()
+            and (
+                self.attn_backend.get_name() == "TRITON_MLA"
+                or self.attn_backend.get_name() == "FLASHINFER"
+            )
+        ):
+            logger.warning_once(
+                "Disabling prefix caching for TRITON_MLA / FLASHINFER "
+                "with batch invariance, as it is not yet supported.",
+                scope="local",
+            )
+            cache_config.enable_prefix_caching = False
+
+        impl_cls = cast(type[DSAAttentionImpl], self.attn_backend.get_impl_cls())
+        self.impl = impl_cls(
+            dim=self.dim,
+            n_heads=self.n_heads,
+            scale=self.scale,
+            n_local_heads=self.n_local_heads,
+            o_lora_rank=self.o_lora_rank,
+            head_dim=self.head_dim,
+            rope_head_dim=self.rope_head_dim,
+            nope_head_dim=self.nope_head_dim,
+            n_groups=self.n_groups,
+            n_local_groups=self.n_local_groups,
+            window_size=self.window_size,
+            compress_ratio=self.compress_ratio,
+            indexer=indexer,
+            **extra_impl_args,
+        )
+
+        self.use_direct_call = not current_platform.opaque_attention_op()
+
+        compilation_config = get_current_vllm_config().compilation_config
+        if prefix in compilation_config.static_forward_context:
+            raise ValueError(f"Duplicate layer name: {prefix}")
+        compilation_config.static_forward_context[prefix] = self
+
+        self.kv_cache = [
+            torch.tensor([])
+            for _ in range(
+                get_current_vllm_config().parallel_config.pipeline_parallel_size
+            )
+        ]
+
+        self.use_sparse = use_sparse
+
+        # Initialize q/k/v range constants.
+        self.q_range = torch.tensor(envs.Q_SCALE_CONSTANT, dtype=torch.float32)
+        self.k_range = torch.tensor(envs.K_SCALE_CONSTANT, dtype=torch.float32)
+        self.v_range = torch.tensor(envs.V_SCALE_CONSTANT, dtype=torch.float32)
+
+    def forward(
+        self,
+        q: torch.Tensor,
+        kv_c_normed: torch.Tensor,
+        k_pe: torch.Tensor,
+        output_shape: torch.Size | None = None,
+    ) -> torch.Tensor:
+        return q
+
+    def process_weights_after_loading(self, act_dtype: torch.dtype):
+        if hasattr(self.impl, "process_weights_after_loading"):
+            self.impl.process_weights_after_loading(act_dtype)
+
+
+    def get_attn_backend(self) -> type[AttentionBackend]:
+        return self.attn_backend
+
+    def get_kv_cache_spec(self, vllm_config: VllmConfig) -> KVCacheSpec:
+        kv_cache_dtype = kv_cache_dtype_str_to_dtype(
+            self.kv_cache_dtype, vllm_config.model_config
+        )
+        return MLAAttentionSpec(
+            block_size=vllm_config.cache_config.block_size,
+            num_kv_heads=1,
+            head_size=self.head_size,
+            dtype=kv_cache_dtype,
+            cache_dtype_str=vllm_config.cache_config.cache_dtype,
+        )
